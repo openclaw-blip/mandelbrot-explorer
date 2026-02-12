23fafb0d68e098d04d46e142fdc0a147230421bd
Sean Rea <sean.rea@gmail.com>
2026-02-12 11:50:21 -0800

    Add 5 more color themes
    - Synthwave: 80s retro purple/pink/orange sunset vibes
    - Aurora: Northern lights greens, teals, and purples
    - Toxic: Radioactive slime greens and yellows
    - Sakura: Soft cherry blossom pinks and whites
    - Noir: Elegant black & white grayscale

--- src/colorThemes.ts
+++ src/colorThemes.ts
@@ -75,6 +75,76 @@ export const colorThemes: ColorTheme[] = [
       [1.0, 1.0, 0.0],      // Yellow
     ],
   },
+  {
+    id: 'synthwave',
+    name: 'Synthwave',
+    colors: [
+      [0.1, 0.0, 0.2],      // Deep purple
+      [0.4, 0.0, 0.6],      // Purple
+      [0.8, 0.0, 0.5],      // Hot magenta
+      [1.0, 0.2, 0.4],      // Neon pink
+      [1.0, 0.5, 0.2],      // Orange
+      [1.0, 0.8, 0.3],      // Golden
+      [0.6, 0.2, 0.8],      // Violet
+      [0.2, 0.0, 0.4],      // Dark purple
+    ],
+  },
+  {
+    id: 'aurora',
+    name: 'Aurora',
+    colors: [
+      [0.0, 0.1, 0.15],     // Night sky
+      [0.0, 0.5, 0.3],      // Deep green
+      [0.1, 0.9, 0.5],      // Bright green
+      [0.3, 1.0, 0.7],      // Mint
+      [0.5, 0.8, 1.0],      // Ice blue
+      [0.6, 0.4, 0.9],      // Violet
+      [0.4, 0.2, 0.6],      // Purple
+      [0.1, 0.3, 0.4],      // Dark teal
+    ],
+  },
+  {
+    id: 'toxic',
+    name: 'Toxic',
+    colors: [
+      [0.0, 0.05, 0.0],     // Near black
+      [0.1, 0.3, 0.0],      // Dark green
+      [0.3, 0.6, 0.0],      // Slime green
+      [0.6, 1.0, 0.0],      // Lime
+      [0.9, 1.0, 0.2],      // Yellow-green
+      [1.0, 0.9, 0.0],      // Yellow
+      [0.7, 0.5, 0.0],      // Mustard
+      [0.2, 0.4, 0.0],      // Olive
+    ],
+  },
+  {
+    id: 'sakura',
+    name: 'Sakura',
+    colors: [
+      [1.0, 0.98, 0.98],    // White
+      [1.0, 0.9, 0.92],     // Pale pink
+      [1.0, 0.75, 0.8],     // Light pink
+      [0.95, 0.55, 0.65],   // Pink
+      [0.85, 0.4, 0.55],    // Rose
+      [0.7, 0.3, 0.45],     // Deep rose
+      [0.9, 0.7, 0.75],     // Dusty pink
+      [1.0, 0.85, 0.88],    // Blush
+    ],
+  },
+  {
+    id: 'noir',
+    name: 'Noir',
+    colors: [
+      [1.0, 1.0, 1.0],      // White
+      [0.85, 0.85, 0.85],   // Light grey
+      [0.65, 0.65, 0.65],   // Medium grey
+      [0.45, 0.45, 0.45],   // Grey
+      [0.3, 0.3, 0.3],      // Dark grey
+      [0.15, 0.15, 0.15],   // Charcoal
+      [0.5, 0.5, 0.5],      // Mid grey
+      [0.75, 0.75, 0.75],   // Silver
+    ],
+  },
 ];
 
 export const defaultTheme = colorThemes[0];
